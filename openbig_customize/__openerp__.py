814b47da4042bd1ed7558087305028c1818d3d1a
Andrzej Grymkowski <andrzej.grymkowski@openglobe.pl>
2013-12-08 18:28:26 +0100

    initial commit with module

---
+++ openbig_customize/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    Module openbig_customize
+#    Copyrigt (C) 2010 OpenGLOBE Andrzej Grymkowski (www.openglobe.pl)
+#                       and big-consulting GmbH (www.openbig.org)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name" : "Openbig Customize",
+    "version" : "1.0 (7.0)",
+    "depends" : ["sale",],
+    "author" : "Thorsten Vocks / Andrzej Grymkowski (openbig.org)",
+    "website" : "www.openbig.org",
+    "description": """
+
+    """,
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data" : [
+        "report/report.xml",
+        ],
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
